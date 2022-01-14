53adb33526fda5020f64854c3931f9378fded85e
zue-dev <zue@kakao.com>
2022-01-14 16:52:16 +0900

    feat: mui 적용

--- src/pages/ConvertFeet.tsx
+++ src/pages/ConvertFeet.tsx
@@ -1,38 +1,40 @@
+import { Box, Input, Typography } from "@mui/material";
 import React, { FC, useState } from "react";
 
 const ConvertFeet: FC = () => {
   const [feetValue, setFeetValue] = useState("");
   const [meterValue, setMeterValue] = useState("");
 
+  const regex = /[^0-9]/g;
+
   const onFeetValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const regex = /[^0-9]/g;
     const value = e.target.value.replace(regex, "");
     setFeetValue(value);
     setMeterValue((Number(value) * 3.305785).toString());
   };
 
   const onMeterValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const regex = /[^0-9]/g;
     const value = e.target.value.replace(regex, "");
     setMeterValue(value);
     setFeetValue((Number(value) * 0.3025).toString());
   };
 
   return (
-    <div>
-      <input
-        value={feetValue}
-        onChange={onFeetValueChange}
-        placeholder="평"
-        type="text"
-      />
-      <input
-        type="text"
-        value={meterValue}
-        onChange={onMeterValueChange}
-        placeholder="m2"
-      />
-    </div>
+    <Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Box display="flex" mb="20px">
+          <Input value={feetValue} onChange={onFeetValueChange} type="text" />
+          <Typography>평</Typography>
+        </Box>
+        <Box display="flex">
+          <Input type="text" value={meterValue} onChange={onMeterValueChange} />
+          <Typography>
+            m<sup>2</sup>
+          </Typography>
+        </Box>
+      </Box>
+      <Typography mt="32px">전용면적이 실평수입니다.</Typography>
+    </Box>
   );
 };
 
